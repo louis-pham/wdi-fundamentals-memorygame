12ccaf7e109bc84c14712089249fe03a3056a0af
louis-pham <l.c.pham@hotmail.com>
2019-11-10 15:36:37 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -30,15 +30,27 @@ function checkForMatch() {
   }
 }
 
-function flipCard(cardId) {
+function flipCard() {
+  var cardId = this.getAttribute("data-id");
   console.log("User flipped " + cards[cardId].rank);
   cardsInPlay.push(cards[cardId].rank);
   console.log(cards[cardId].file);
   console.log(cards[cardId].suit);
+  this.setAttribute("src", cards[cardId].file);
   if (cardsInPlay.length === 2) {
     checkForMatch();
   }
 }
 
-flipCard(0);
-flipCard(2);
+function createBoard() {
+  var gameBoard = document.querySelector("#game-board");
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute("src", "images/back.png");
+    cardElement.setAttribute("data-id", i);
+    cardElement.addEventListener("click", flipCard);
+    gameBoard.appendChild(cardElement);
+  }
+}
+
+createBoard();
